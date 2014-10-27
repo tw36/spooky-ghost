2352e9bf56ec4d6c64daa85b5473c107b66fae0d
twahl36 <t.ww2@aol.com>
2014-10-27 00:11:54 -0400

    changed again button position

--- build/clumsy.js
+++ build/clumsy.js
@@ -584,7 +584,7 @@ game.GameOverScreen = me.ScreenObject.extend({
 		 var gImage1 = me.loader.getImage('again');
         me.game.world.addChild(new me.SpriteObject(
                 me.video.getWidth()/2 - gImage1.width/2,
-                me.video.getHeight()/2 - gImage1.height/2 - 5,
+                me.video.getHeight()/2 - gImage1.height/2 + 100,
                 gImage1
         ), 12);
 
